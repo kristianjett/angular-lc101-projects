55e2fa8d00e639719e87725b245d4b88fd00eb8e
Kristian <jettkristian@gmail.com>
2020-09-17 00:03:39 -0700

    completed exercise up to adding button movement.

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,4 +12,36 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+
+  handleTakeOff() {
+    let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
+    if (result) {
+      this.color = 'blue';
+      this.height = 10000;
+      this.width = 0;
+      this.message = 'Shuttle in flight.';
+    }
+  }
+
+  handleLanding() {
+    if (this.color === 'blue' && this.height > 0) {
+      window.alert('Prepare for landing...');
+      this.color = 'green';
+      this.height = 0;
+      this.width = 0;
+      this.message = 'Shuttle has landed.';
+    }
+  }
+
+  handleAbortMission() {
+    if (this.color === 'blue' && this.height > 0) {
+      let result = window.confirm('Are you sure you want to abort mission?');
+      if (result){
+        this.color = 'green';
+        this.height = 0;
+        this.width = 0;
+        this.message = 'Mission aborted. Shuttle has landed.';
+      }
+    }
+  }
 }
